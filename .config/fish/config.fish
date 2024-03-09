ae34041a0e277df937435d5690ad2d87cdd65e8b
sirlucidae <lucidae@outlook.com.au>
2024-03-09 19:46:31 +1000

    Added/removed some files

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+set -U fish_greeting
+#function fish_greeting
+#echo The time is (set_color yellow; date +%T; set_color normal) and this machine is called $hostname
+end
